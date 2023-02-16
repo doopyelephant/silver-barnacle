6ab677d91729516ffe2f31ef01f312d6ba9e2248
doopyelephant <91019070+doopyelephant@users.noreply.github.com>
2023-02-16 16:21:04 +0000

    Added template "WPF"

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace silver_barnacle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace silver_barnacle
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
